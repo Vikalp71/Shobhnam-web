f41bf715a9c749b99e46cacdc3069ab1b1f0d68b
shobhnam412 <shobhnam412@gmail.com>
2026-04-17 22:29:55 +0530

    Fix formatting in contact route component

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <Route path="/contact" element={
         <>
           <Header />
-          <Contact />  // Contact 
+          <Contact />  
           <Footer />
         </>
       } />
