97d8195388e3b7f21f8239f17273d449af211684
vikalp bajpai <bajpaivikalp792@gmail.com>
2026-04-18 18:38:33 +0530

    artist images changes

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,14 +1,19 @@
 import './Artists.css';
+import RamleelaImg from '../images/Ramleela.png';
+import SunderkandImg from '../images/Sunderkand.jpeg';
+import BhagwatImg from '../images/bhagwat.jpeg';
+import BhajanImg from '../images/Bhajan.png';
+import RudrabhishekImg from '../images/Rudrabhishek copy.jpg';
 
 const artists = [
-  { img: 'src/images/Ramleela.png', name: 'Ram Narayan Mishra', service: 'Ramleela' },
-  { img: 'src/images/Sunderkand.jpeg', name: 'Pandit Anil Bajpai', service: 'Sunderkand' },
-  { img: 'src/images/bhagwat.jpeg', name: 'Pandit Anil Dubey', service: 'Bhagwat Katha' },
-  { img: 'src/images/Bhajan.png', name: 'Pandit Malay Pandey', service: 'Bhajan Sandhya' },
-  { img: 'src/images/Rudrabhishek copy.jpeg', name: 'Pandit Rajan Bajpai', service: 'Rudrabhishek' },
-  { img: 'src/images/bhagwat.jpeg', name: 'Pandit Rishabh', service: 'Bhagwat Katha' },
-  { img: 'src/images/Sunderkand.jpeg', name: 'Pandit Anil Bajpai', service: 'Sunderkand' },
-  { img: 'src/images/Ramleela.png', name: 'Pandit Ram Narayan Mishra', service: 'Ramleela' },
+  { img: RamleelaImg, name: 'Ram Narayan Mishra', service: 'Ramleela' },
+  { img: SunderkandImg, name: 'Pandit Anil Bajpai', service: 'Sunderkand' },
+  { img: BhagwatImg, name: 'Pandit Anil Dubey', service: 'Bhagwat Katha' },
+  { img: BhajanImg, name: 'Pandit Malay Pandey', service: 'Bhajan Sandhya' },
+  { img: RudrabhishekImg, name: 'Pandit Rajan Bajpai', service: 'Rudrabhishek' },
+  { img: BhagwatImg, name: 'Pandit Rishabh', service: 'Bhagwat Katha' },
+  { img: SunderkandImg, name: 'Pandit Anil Bajpai', service: 'Sunderkand' },
+  { img: RamleelaImg, name: 'Pandit Ram Narayan Mishra', service: 'Ramleela' },
 ];
 
 const scrollItems = [...artists, ...artists];
@@ -23,7 +28,7 @@ function Artists() {
             <div className="artist-card" key={index}>
               <img src={artist.img} alt={artist.name} />
               <h4>{artist.name}</h4>
-              <p>{artist.service}</p>
+              <p>{artist.service}</p> 
             </div>
           ))}
         </div>
